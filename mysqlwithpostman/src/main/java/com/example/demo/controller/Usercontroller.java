c83da00d1f1e6f561ad25f35d01ad15d6cf05c2f
nisrac <116939439+nisrac@users.noreply.github.com>
2023-01-11 15:04:39 +0530

    Add files via upload

---
+++ mysqlwithpostman/src/main/java/com/example/demo/controller/Usercontroller.java
@@ -0,0 +1,51 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.Entity.User;
+import com.example.demo.Repository.UserRepository;
+
+@RestController
+@RequestMapping("/users")
+public class Usercontroller {
+@Autowired
+private UserRepository userRepository;
+
+@GetMapping
+public List<User> getAllUser()
+{
+	return this.userRepository.findAll();
+}
+
+@GetMapping("/{id}")
+public User getUserById(@PathVariable(value="id") long userId) {
+	return this.userRepository.findById(userId)
+			.orElseThrow();
+
+}
+@PostMapping
+public User createUser(@RequestBody User user)
+{
+	return this.userRepository.save(user);
+}
+@PutMapping("/{id}")
+public User updateUser(@RequestBody User user,@PathVariable("id") long userId)
+{
+	//return this.userRepository.save(user);
+	User ex=this.userRepository.findById(userId)
+			.orElseThrow();
+	ex.setFirstname(user.getFirstname());
+	ex.setLasttname(user.getLasttname());
+	return this.userRepository.save(ex);
+}
+}
